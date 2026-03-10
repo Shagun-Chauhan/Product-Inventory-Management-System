8d9f14bd8f5a96d9ace906b2291eeb244961da66
Samruddhi3802 <sahindrakarsamruddhi01@gmail.com>
2026-03-10 14:15:40 +0530

    getallproducts and delete functionality

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -77,4 +77,51 @@ const getOneProduct = async (req,res)=>{
     })
 }
 }
-module.exports = {addProduct,updateProduct,getOneProduct}
+
+const getAllProducts = async(req, res)=>{
+    try {
+        let productData= await products.find({});
+        if(productData){
+            return res.status(200).json({
+                success:true,
+                message:"Products found successfully",
+                productData
+            })
+        }
+        return res.status(400).json({
+            success:false,
+            message:"No products found"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
+const deleteProduct = async (req, res)=>{
+    try{
+       const data=req.body;
+       let response=await products.deleteOne({productName : data.productName});
+       if(response.deletedCount > 0){
+        return res.status(200).json({
+            success:true,
+            message: "Product deleted successfully",
+            totalCount : response.deletedCount
+        })
+    }
+    return res.status(400).json({
+        success:false,
+        message : "Product not found"
+    })
+}
+catch(err){
+    return res.status(500).json({
+        success:false,
+        message : err.message
+    })
+}
+}
+
+module.exports = {addProduct,updateProduct,getOneProduct,getAllProducts,deleteProduct}
